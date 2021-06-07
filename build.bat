d680130d830ba8eec78df76c1515ed7cb137c8f1
Andrey Podlesnyy <podlesniy@armo.ru>
2021-06-07 14:32:11 +0300

    fix build myget

--- build.bat
+++ build.bat
@@ -9,12 +9,12 @@ if not "%PackageVersion%" == "" (
    set version=-Version %PackageVersion%
 )
 
-%nuget% restore Source\Main.sln -NoCache -NonInteractive
-"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" Source\Main.sln /p:Configuration="%config%" /p:Platform="Any CPU" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+"%nuget%" restore Source\Main.sln -NoCache -NonInteractive
+"%ProgramFiles(x86)%\MSBuild\16.0\Bin\MSBuild.exe" Source\Main.sln /p:Configuration="%config%" /p:Platform="Any CPU" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 cd Packs
 
-set firstPartNuget=%nuget% pack 
+set firstPartNuget="%nuget%" pack 
 set lastPartNuget= -NonInteractive %version%
 
 cmd /c %firstPartNuget%GenerateTimexReferences.nuspec%lastPartNuget%
